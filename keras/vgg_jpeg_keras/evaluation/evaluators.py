a2454b9ab64a7f9c16abf4c600c4a2bd21342440
BenjaminDeguerre <benjamin.deguerre@insa-rouen.fr>
2019-09-17 17:17:44 +0200

    formatting

--- keras/vgg_jpeg_keras/evaluation/evaluators.py
+++ keras/vgg_jpeg_keras/evaluation/evaluators.py
@@ -2,8 +2,8 @@ from template_keras.evaluators import TemplateEvaluator
 
 import numpy as np
 
-class Evaluator(TemplateEvaluator):
 
+class Evaluator(TemplateEvaluator):
     def __init__(self, generator=None):
         self.score = None
         self._generator = generator
@@ -12,17 +12,21 @@ class Evaluator(TemplateEvaluator):
 
     def __call__(self, model, test_generator=None):
         if self._generator is None and test_generator is None:
-            raise RuntimeError("A generator should be specified using the init or parameters.")
+            raise RuntimeError(
+                "A generator should be specified using the init or parameters."
+            )
         self.runs = False
         if test_generator is not None:
             self._generator = test_generator
 
-        self.score = model.evaluate_generator(self._generator, verbose=1, steps=50)
+        self.score = model.evaluate_generator(self._generator, verbose=1)
 
     def make_runs(self, model, test_generator=None, number_of_runs=10):
 
         if self._generator is None and test_generator is None:
-            raise RuntimeError("A generator should be specified using the init or parameters.")
+            raise RuntimeError(
+                "A generator should be specified using the init or parameters."
+            )
 
         scores = []
         self.runs = True
@@ -42,13 +46,14 @@ class Evaluator(TemplateEvaluator):
 
     def __str__(self):
         if self.runs:
-            return "Number of runs: {}\nAverage score: {}".format(self.number_of_runs, self.score)
+            return "Number of runs: {}\nAverage score: {}".format(
+                self.number_of_runs, self.score)
         else:
             return "The evaluated score is {}.".format(self.score)
 
     @property
     def test_generator(self):
         return self._generator
-    
+
     def display_results(self):
         pass
