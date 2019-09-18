1d016c75018438294f619249b6fedff60114dd36
benjamin deguerre <benjamin.deguerre@actemium.com>
2019-09-18 13:34:45 +0200

    empty displayer

--- keras/vgg_jpeg_keras/displayer/displayers.py
+++ keras/vgg_jpeg_keras/displayer/displayers.py
@@ -1,20 +1,12 @@
-#import matplotlib.pyplot as plt
+from template_keras.displayers import TemplateDisplayer
 
-# from template.displayer import TemplateDisplayer
-
-class Displayer(object):
+class Displayer(TemplateDisplayer):
 
     def __init__(self):
         pass
 
-    # def display_with_gt(self, predictions, inputs, groundtruth):
-    #     for i, gt in enumerate(groundtruth):
-    #         plt.title('Ground Truth : {}\nPrediction: {}'.format(gt.argmax(), predictions[i].argmax()))
-    #         plt.imshow(inputs[i][:,:,0])
-    #         plt.show()
+    def display(self, predictions, inputs):
+        pass
 
-    # def display(self, predictions, inputs):
-    #     for i, prediction in enumerate(predictions):
-    #         plt.title('Prediction: {}'.format(prediction.argmax()))
-    #         plt.imshow(inputs[i][:,:,0])
-    #         plt.show()
+    def display_with_gt(self, predictions, inputs, groundtruth):
+        pass
