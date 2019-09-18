789b2ba700747a3b2f463b64531b0cd635a3b780
benjamin deguerre <benjamin.deguerre@actemium.com>
2019-09-18 14:45:52 +0200

    updating the training file

--- keras/training.py
+++ keras/training.py
@@ -17,16 +17,16 @@ import csv
 
 from operator import itemgetter
 
-from comet_ml import Experiment
-
 import keras.backend as K
 
 import tensorflow as tf
 import horovod.keras as hvd
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-r', '--restart', help="Restart the training from a previous stopped config. The argument is the path to the experiment folder.", type=str)
-parser.add_argument('-c', '--configuration', help="Path to the directory containing the config file to use. The configuration file should be named 'config_file.py' (see the examples in the config folder of the repository).")
+parser.add_argument(
+    '-r', '--restart', help="Restart the training from a previous stopped config. The argument is the path to the experiment folder.", type=str)
+parser.add_argument('-c', '--configuration',
+                    help="Path to the directory containing the config file to use. The configuration file should be named 'config_file.py' (see the examples in the config folder of the repository).")
 parser.add_argument('--horovod', dest='horovod', action='store_true')
 args = parser.parse_args()
 
@@ -50,17 +50,14 @@ if args.restart is not None:
     config = TrainingConfiguration()
     key = dirname(args.restart).split("_")[-1]
 
-    # The results will be saved in an other directory not to pollute the original one.
-    key += "_temp"
-
     # We extract the last saved weight and the corresponding epoch
     weights_path = join(args.restart, "checkpoints")
-    weights_files = sorted([[f, int(f.split('_')[0].split('-')[1])] for f in listdir(weights_path) if isfile(join(weights_path, f))], key=itemgetter(1))
-
+    weights_files = sorted([[f, int(f.split('_')[0].split('-')[1])] for f in listdir(
+        weights_path) if isfile(join(weights_path, f))], key=itemgetter(1))
 
     config.weights = join(weights_path, weights_files[-1][0])
     restart_epoch = weights_files[-1][1]
-    
+
     # We load the results file to get the epoch learning rate.
     with open(join(args.restart, 'results/results.csv'), newline='') as csvfile:
         results = csv.reader(csvfile, delimiter=',')
@@ -81,19 +78,19 @@ else:
     # Starting the experiment
 
     key = ''.join(random.choice(string.ascii_uppercase +
-                                    string.ascii_lowercase + string.digits) for _ in range(32))
+                                string.ascii_lowercase + string.digits) for _ in range(32))
 
     output_dir = "{}_{}_{}".format(config.workspace, config.project_name, key)
 
-if args.horovod and hvd.rank() == 0:
-    output_dir = join(environ["EXPERIMENTS_OUTPUT_DIRECTORY"], "{}_{}_{}".format(config.workspace, config.project_name, key))
+if (args.horovod and hvd.rank() == 0) or (not args.horovod):
+    output_dir = join(environ["EXPERIMENTS_OUTPUT_DIRECTORY"], "{}_{}_{}".format(
+        config.workspace, config.project_name, key))
 
     checkpoints_output_dir = join(output_dir, "checkpoints")
     config_output_dir = join(output_dir, "config")
     results_output_dir = join(output_dir, "results")
 
     # We create all the output directories
-
     makedirs(output_dir, exist_ok=True)
     makedirs(checkpoints_output_dir, exist_ok=True)
     makedirs(config_output_dir, exist_ok=True)
@@ -103,30 +100,35 @@ if args.horovod and hvd.rank() == 0:
 if args.horovod:
     config.prepare_horovod(hvd)
 
-if args.horovod and hvd.rank() == 0:
+if (args.horovod and hvd.rank() == 0) or (not args.horovod):
     config.add_csv_logger(results_output_dir)
     config.add_model_checkpoint(checkpoints_output_dir)
 
 # Saving the config file.
 if args.restart is None:
     if args.horovod and hvd.rank() == 0:
-        copyfile(join(args.configuration, "config_file.py"), join(config_output_dir, "saved_config.py"))
-        copyfile(join(args.configuration, "config_file.py"), join(config_output_dir, "temp_config.py"))
+        copyfile(join(args.configuration, "config_file.py"),
+                 join(config_output_dir, "saved_config.py"))
+        copyfile(join(args.configuration, "config_file.py"),
+                 join(config_output_dir, "temp_config.py"))
 else:
     if args.horovod and hvd.rank() == 0:
-        copyfile(join(args.restart, "config/saved_config.py"), join(config_output_dir, "saved_config.py"))
-        copyfile(join(args.restart, "config/saved_config.py"), join(config_output_dir, "temp_config.py"))
+        copyfile(join(args.restart, "config/saved_config.py"),
+                 join(config_output_dir, "saved_config.py"))
+        copyfile(join(args.restart, "config/saved_config.py"),
+                 join(config_output_dir, "temp_config.py"))
 
 # Creating the model
 model = config.network
 
-if config.weights is not None and args.horovod and hvd.rank() == 0:
+if config.weights is not None and args.horovod and hvd.rank() == 0 or config.weights is not None and not args.horovod:
     print("Loading weights (by name): {}".format(config.weights))
     model.load_weights(config.weights, by_name=True)
 
 # Setting the iteration variable if restarting the training.
 if args.restart is not None:
-    config.optimizer.iterations = K.variable(config.steps_per_epoch * restart_epoch, dtype='int64', name='iterations')
+    config.optimizer.iterations = K.variable(
+        config.steps_per_epoch * restart_epoch, dtype='int64', name='iterations')
     config.optimizer.lr = K.variable(restart_lr, name='lr')
 
 # Prepare the generators
@@ -134,8 +136,8 @@ config.prepare_training_generators()
 
 # Compiling the model
 model.compile(loss=config.loss,
-                  optimizer=config.optimizer,
-                  metrics=config.metrics)
+              optimizer=config.optimizer,
+              metrics=config.metrics)
 
 if restart_epoch is not None:
     model.fit_generator(config.train_generator,
@@ -145,6 +147,7 @@ if restart_epoch is not None:
                         callbacks=config.callbacks,
                         workers=config.workers,
                         use_multiprocessing=config.multiprocessing,
+                        validation_steps=config.validation_steps,
                         initial_epoch=restart_epoch)
 else:
     # Fit the model on the batches generated by datagen.flow().
