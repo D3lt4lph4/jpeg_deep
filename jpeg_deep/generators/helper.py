51a876c4d5b8ae42e8203eaf2249a11f814a828a
benjamin deguerre <benjamin.deguerre@actemium.com>
2019-10-22 15:20:20 +0200

    adding helper function to parse the xmls

--- jpeg_deep/generators/helper.py
+++ jpeg_deep/generators/helper.py
@@ -54,3 +54,60 @@ def vertical_flip(img, y, vflip_prob=0.5):
         img = img[::-1]
         y[:, [1, 3]] = 1 - y[:, [3, 1]]
     return img, y
+
+
+def parse_xml_voc(data_path: str, classes: List[str] = None):
+    '''
+    This is an XML parser for datasets in the Pascal VOC format.
+
+    # Arguments:
+        - data_path: A list of strings, where each string is the path of a directory that
+        - classes (list, optional): A list containing the names of the object classes as found in the
+            `name` XML tags. Must include the class `background` as the first list item. The order of this list
+            defines the class IDs.
+        - exclude_difficult (bool, optional): If `True`, excludes boxes that are labeled as 'difficult'.
+
+    # Returns:
+
+    '''
+    if classes is None:
+        classes = ['background',
+                   'aeroplane', 'bicycle', 'bird', 'boat',
+                   'bottle', 'bus', 'car', 'cat',
+                   'chair', 'cow', 'diningtable', 'dog',
+                   'horse', 'motorbike', 'person', 'pottedplant',
+                   'sheep', 'sofa', 'train', 'tvmonitor']
+    boxes = []
+    eval_difficult = []
+
+    with open(os.path.join(annotations_dir, image_id + '.xml')) as f:
+        soup = BeautifulSoup(f, 'xml')
+
+    folder = soup.folder.text
+
+    # Get a list of all objects in this image.
+    objects = soup.find_all('object')
+
+    # Parse the data for each object.
+    for obj in objects:
+        class_name = obj.find('name', recursive=False).text
+        class_id = self.classes.index(class_name)
+        truncated = int(
+            obj.find('truncated', recursive=False).text)
+        difficult = int(
+            obj.find('difficult', recursive=False).text)
+
+        # Get the bounding box coordinates.
+        bndbox = obj.find('bndbox', recursive=False)
+        xmin = int(bndbox.xmin.text)
+        ymin = int(bndbox.ymin.text)
+        xmax = int(bndbox.xmax.text)
+        ymax = int(bndbox.ymax.text)
+
+        boxes.append([class_id, xmin, ymin, xmax, ymax])
+        if difficult:
+            eval_difficult.append(True)
+        else:
+            eval_difficult.append(False)
+
+    return boxes, eval_difficult
