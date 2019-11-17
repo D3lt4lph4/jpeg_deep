84c5ab772a25964c60c363cc1e26442f8b5daf84
benjamin deguerre <benjamin.deguerre@actemium.com>
2019-11-17 22:28:42 +0100

    up

--- config/vgg/vggA_dct/config_file.py
+++ config/vgg/vggA_dct/config_file.py
@@ -6,9 +6,9 @@ from keras.losses import categorical_crossentropy
 from keras.callbacks import ModelCheckpoint, TerminateOnNaN, CSVLogger, EarlyStopping, ReduceLROnPlateau
 from keras.metrics import top_k_categorical_accuracy
 
-from vgg_jpeg_keras.networks import vgga_dct
-from vgg_jpeg_keras.evaluation import Evaluator
-from vgg_jpeg_keras.generators import DCTGeneratorJPEG2DCT
+from jpeg_deep.networks import vgga_dct
+from jpeg_deep.evaluation import Evaluator
+from jpeg_deep.generators import DCTGeneratorJPEG2DCT
 
 from template_keras.config import TemplateConfiguration
 
