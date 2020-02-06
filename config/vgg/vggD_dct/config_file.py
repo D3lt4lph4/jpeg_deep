63298ed672b12a760058d45e367a6d35b017f72a
BenjaminDeguerre <benjamin.deguerre@insa-rouen.fr>
2020-02-06 16:50:02 +0100

    updating the config files

--- config/vgg/vggD_dct/config_file.py
+++ config/vgg/vggD_dct/config_file.py
@@ -3,34 +3,17 @@ from os.path import join
 
 from keras.optimizers import SGD
 from keras.losses import categorical_crossentropy
-from keras.callbacks import ModelCheckpoint, TerminateOnNaN, CSVLogger, EarlyStopping, ReduceLROnPlateau
-from keras.metrics import top_k_categorical_accuracy
+from keras.callbacks import ModelCheckpoint, TerminateOnNaN, EarlyStopping, ReduceLROnPlateau, TensorBoard
 
 from jpeg_deep.networks import vggd_dct
 from jpeg_deep.evaluation import Evaluator
 from jpeg_deep.generators import DCTGeneratorJPEG2DCT
 
-from template_keras.config import TemplateConfiguration
-
-
 from albumentations import (
-    Blur,
     HorizontalFlip,
     RandomCrop,
-    RandomGamma,
-    Rotate,
-    OpticalDistortion,
-    GridDistortion,
-    ElasticTransform,
-    HueSaturationValue,
-    RandomBrightness,
-    RandomContrast,
-    MotionBlur,
-    MedianBlur,
-    GaussianBlur,
-    ChannelShuffle,
-    SmallestMaxSize,
-    RandomBrightnessContrast
+    CenterCrop,
+    SmallestMaxSize
 )
 
 from albumentations import (
@@ -39,13 +22,7 @@ from albumentations import (
 )
 
 
-def _top_k_accuracy(k):
-    def _func(y_true, y_pred):
-        return top_k_categorical_accuracy(y_true, y_pred, k)
-    return _func
-
-
-class TrainingConfiguration(TemplateConfiguration):
+class TrainingConfiguration(object):
     def __init__(self):
         # Variables to hold the description of the experiment
         self.description = "Training configuration file for the DCT version of the  VGGA network."
@@ -53,48 +30,40 @@ class TrainingConfiguration(TemplateConfiguration):
         # System dependent variable
         self._workers = 5
         self._multiprocessing = True
-        self._gpus = 1
 
         # Variables for comet.ml
         self._project_name = "jpeg_deep"
         self._workspace = "classification_dct"
 
         # Network variables
-        self.num_classes = 1000
-        self.img_size = (224, 224)
-        self._weights = "/dlocal/home/2017018/bdegue01/experiments/d3lt4lph4_vgg-dct_Dy1oTqxW4ZuUGUJ7p5t8f68NVaX7K9P3/checkpoints/epoch-57_loss-1.5516_val_loss-2.1839.h5"
+        self._weights = None
         self._network = vggd_dct(self.num_classes)
 
         # Training variables
-        self._epochs = 120
-        self._batch_size = 256
-        self.batch_size_divider = 2
-        self._steps_per_epoch = 5000
+        self._epochs = 180
+        self._batch_size = 64
+        self._steps_per_epoch = 1281167 // self._batch_size
         self._validation_steps = 50000 // self._batch_size
         self.optimizer_parameters = {
-            "lr": 0.01, "momentum": 0.9, "decay": 0, "nesterov": True}
+            "lr": 0.0025, "momentum": 0.9}
         self._optimizer = SGD(**self.optimizer_parameters)
         self._loss = categorical_crossentropy
-        self._metrics = [_top_k_accuracy(1), _top_k_accuracy(5)]
+        self._metrics = ['accuracy', 'top_k_categorical_accuracy']
+        
         self.train_directory = join(
             environ["DATASET_PATH_TRAIN"], "imagenet/train")
         self.validation_directory = join(
             environ["DATASET_PATH_VAL"], "imagenet/validation")
-        self.index_file = "/home/2017018/bdegue01/git/vgg_jpeg/data/imagenet_class_index.json"
+        self.test_directory = join(
+            environ["DATASET_PATH_VAL"], "imagenet/validation")
+        self.validation_split = 0.95
+        self.index_file = "/home/2017018/bdegue01/git/jpeg_deep/data/imagenet_class_index.json"
 
         # Defining the transformations that will be applied to the inputs.
         self.train_transformations = [
-            OneOf([
-                OneOf([Blur(), MotionBlur(), MedianBlur(),
-                       GaussianBlur()], p=0.5),
-                OneOf([HueSaturationValue(),
-                       RandomBrightness(), RandomContrast(), RandomGamma()]),
-
-            ], p=0.5),
-            OneOf([HorizontalFlip(), Rotate(limit=25), OneOf(
-                [OpticalDistortion(), GridDistortion(), ElasticTransform()], p=0.5)], p=0.5),
             SmallestMaxSize(256),
-            RandomCrop(224, 224)
+            RandomCrop(224, 224),
+            HorizontalFlip()
         ]
 
         self.validation_transformations = [
@@ -102,12 +71,11 @@ class TrainingConfiguration(TemplateConfiguration):
 
         # Keras stuff
         self.model_checkpoint = None
-        self.csv_logger = None
         self.reduce_lr_on_plateau = ReduceLROnPlateau(patience=5, verbose=1)
         self.terminate_on_nan = TerminateOnNaN()
         self.early_stopping = EarlyStopping(monitor='val_loss',
                                             min_delta=0,
-                                            patience=10)
+                                            patience=11)
 
         self._callbacks = [self.reduce_lr_on_plateau,
                            self.terminate_on_nan, self.early_stopping]
@@ -115,45 +83,29 @@ class TrainingConfiguration(TemplateConfiguration):
         # Creating the training and validation generator
         self._train_generator = None
         self._validation_generator = None
+        self._test_generator = None
 
         self._horovod = None
 
-    def add_csv_logger(self,
-                       output_path,
-                       filename="results.csv",
-                       separator=',',
-                       append=True):
-        if self.horovod is not None:
-            if self.horovod.rank() == 0:
-                self.csv_logger = CSVLogger(filename=join(output_path, filename),
-                                            separator=separator,
-                                            append=append)
-                self._callbacks.append(self.csv_logger)
-        else:
-            self.csv_logger = CSVLogger(filename=join(output_path, filename),
-                                        separator=separator,
-                                        append=append)
-            self._callbacks.append(self.csv_logger)
+    def prepare_runtime_checkpoints(self, directories_dir):
+        log_dir = directories_dir["log_dir"]
+        checkpoints_dir = directories_dir["checkpoints_dir"]
 
-    def add_model_checkpoint(self, output_path, verbose=1,
-                             save_best_only=True):
-        if self.horovod is not None:
-            if self.horovod.rank() == 0:
-                self._callbacks.append(ModelCheckpoint(filepath=join(
-                    output_path,
-                    "epoch-{epoch:02d}_loss-{loss:.4f}_val_loss-{val_loss:.4f}.h5"),
-                    verbose=verbose,
-                    save_best_only=save_best_only))
-        else:
-            self.model_checkpoint = ModelCheckpoint(filepath=join(
-                output_path,
-                "epoch-{epoch:02d}_loss-{loss:.4f}_val_loss-{val_loss:.4f}.h5"),
-                verbose=verbose,
-                save_best_only=save_best_only)
-            self._callbacks.append(self.model_checkpoint)
+        self._callbacks.append(ModelCheckpoint(filepath=join(
+            checkpoints_dir,
+            "epoch-{epoch:02d}_loss-{loss:.4f}_val_loss-{val_loss:.4f}.h5"),
+            save_best_only=True))
+        self._callbacks.append(
+            TensorBoard(log_dir))
 
     def prepare_horovod(self, hvd):
         self._horovod = hvd
+        self.optimizer_parameters["lr"] = self.optimizer_parameters["lr"] * hvd.size()
+        self._optimizer = SGD(**self.optimizer_parameters)
+        self._optimizer = hvd.DistributedOptimizer(self._optimizer)
+        self._steps_per_epoch = self._steps_per_epoch // hvd.size()
+        self._validation_steps = 3 * self._validation_steps // hvd.size()
+
         self._callbacks = [
             hvd.callbacks.BroadcastGlobalVariablesCallback(0),
 
@@ -175,14 +127,6 @@ class TrainingConfiguration(TemplateConfiguration):
             self.early_stopping
         ]
 
-        self.optimizer_parameters["lr"] = self.optimizer_parameters["lr"] * \
-            hvd.size() / self.batch_size_divider
-        self._optimizer = hvd.DistributedOptimizer(self._optimizer)
-        self._batch_size = self._batch_size // self.batch_size_divider
-        self._steps_per_epoch = self._steps_per_epoch // (
-            hvd.size() // self.batch_size_divider)
-        self._validation_steps = 3 * self._validation_steps // hvd.size()
-
     def prepare_for_inference(self):
         pass
 
@@ -190,14 +134,14 @@ class TrainingConfiguration(TemplateConfiguration):
         self._evaluator = Evaluator()
 
     def prepare_testing_generator(self):
-        pass
+        self._test_generator = DCTGeneratorJPEG2DCT(
+            self.test_directory, self.index_file, None, 1)
 
     def prepare_training_generators(self):
         self._train_generator = DCTGeneratorJPEG2DCT(
-            self.train_directory, self.index_file, self._batch_size, scale=True)
-
+            self.train_directory, self.index_file, batch_size=self.batch_size, shuffle=True, transforms=self.train_transformations)
         self._validation_generator = DCTGeneratorJPEG2DCT(
-            self.validation_directory, self.index_file, self._batch_size, scale=False)
+            self.validation_directory, self.index_file, batch_size=self.batch_size, shuffle=True, transforms=self.validation_transformations)
 
     @property
     def train_generator(self):
