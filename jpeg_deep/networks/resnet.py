62851e723bce9d4a6606f5d865dbc13c9456a33c
BenjaminDeguerre <benjamin.deguerre@insa-rouen.fr>
2020-02-13 21:06:43 +0100

    adding DCT

--- jpeg_deep/networks/resnet.py
+++ jpeg_deep/networks/resnet.py
@@ -147,6 +147,7 @@ def ResNet50(classes=1000):
     else:
         bn_axis = 1
 
+    # 1
     x = layers.ZeroPadding2D(padding=(3, 3), name='conv1_pad')(img_input)
     x = layers.Conv2D(64, (7, 7),
                       strides=(2, 2),
@@ -160,15 +161,18 @@ def ResNet50(classes=1000):
     x = layers.ZeroPadding2D(padding=(1, 1), name='pool1_pad')(x)
     x = layers.MaxPooling2D((3, 3), strides=(2, 2))(x)
 
+    # 2
     x = conv_block(x, 3, [64, 64, 256], stage=2, block='a', strides=(1, 1))
     x = identity_block(x, 3, [64, 64, 256], stage=2, block='b')
     x = identity_block(x, 3, [64, 64, 256], stage=2, block='c')
 
+    # 3
     x = conv_block(x, 3, [128, 128, 512], stage=3, block='a')
     x = identity_block(x, 3, [128, 128, 512], stage=3, block='b')
     x = identity_block(x, 3, [128, 128, 512], stage=3, block='c')
     x = identity_block(x, 3, [128, 128, 512], stage=3, block='d')
 
+    # 4
     x = conv_block(x, 3, [256, 256, 1024], stage=4, block='a')
     x = identity_block(x, 3, [256, 256, 1024], stage=4, block='b')
     x = identity_block(x, 3, [256, 256, 1024], stage=4, block='c')
@@ -176,6 +180,7 @@ def ResNet50(classes=1000):
     x = identity_block(x, 3, [256, 256, 1024], stage=4, block='e')
     x = identity_block(x, 3, [256, 256, 1024], stage=4, block='f')
 
+    # 5
     x = conv_block(x, 3, [512, 512, 2048], stage=5, block='a')
     x = identity_block(x, 3, [512, 512, 2048], stage=5, block='b')
     x = identity_block(x, 3, [512, 512, 2048], stage=5, block='c')
@@ -248,3 +253,111 @@ def ResNet50_fcn(classes=1000):
     model = models.Model(inputs, x, name='resnet50')
 
     return model
+
+
+def late_concat_rfa():
+    input_shape_y = (28, 28, 64)
+    input_shape_cbcr = (14, 14, 128)
+
+    Y = layers.Input(shape=input_shape_y)
+    CbCr = layers.Input(shape=input_shape_cbcr)
+
+    if backend.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+
+    bn_Y = layers.BatchNormalization(
+        axis=bn_axis, momentum=0.9, epsilon=1e-5, name='bn_y')(Y)
+
+    y = conv_block(bn_Y, 1, [256, 256, 1024],
+                   stage=1, block="a", strides=(1, 1))
+    y = identity_block(y, 2, [256, 256, 1024], stage=1, block="b")
+    y = identity_block(y, 3, [256, 256, 1024], stage=1, block="c")
+
+    y = conv_block(y, 3, [128, 128, 512], stage=2, block='a', strides=(1, 1))
+    y = identity_block(y, 3, [128, 128, 512], stage=2, block='b')
+    y = identity_block(y, 3, [128, 128, 512], stage=2, block='c')
+    y = identity_block(y, 3, [128, 128, 512], stage=2, block='d')
+
+    y = conv_block(y, 3, [256, 256, 512], stage=2, block='a4')
+
+    bn_CbCr = layers.BatchNormalization(
+        axis=bn_axis, momentum=0.9, epsilon=1e-5, name='bn_cbcr')(CbCr)
+    cbcr = conv_block(bn_CbCr, 1, [256, 256, 1024],
+                      stage=2, block='a5', strides=(1, 1))
+
+    x = Concatenate(axis=-1)([y, cbcr])
+
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='b')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='c')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='d')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='e')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='f')
+
+    x = conv_block(x, 3, [512, 512, 2048], stage=5, block='a')
+    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='b')
+    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='c')
+
+    x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
+    x = layers.Dense(classes, activation='softmax',
+                     kernel_regularizer=l2(0.00005), name='fc1000')(x)
+
+    inputs = [img_input, img_input]
+
+    # Create model.
+    model = models.Model(inputs, x, name='resnet50_late_concat_rfa')
+
+
+def late_concat_rfa_thinner():
+    input_shape_y = (28, 28, 64)
+    input_shape_cbcr = (14, 14, 128)
+
+    Y = layers.Input(shape=input_shape_y)
+    CbCr = layers.Input(shape=input_shape_cbcr)
+
+    if backend.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+
+    bn_Y = layers.BatchNormalization(
+        axis=bn_axis, momentum=0.9, epsilon=1e-5, name='bn_y')(Y)
+
+    y = conv_block(bn_Y, 1, [256, 256, 384],
+                   stage=1, block="a", strides=(1, 1))
+    y = identity_block(y, 2, [256, 256, 1024], stage=1, block="b")
+    y = identity_block(y, 3, [256, 256, 1024], stage=1, block="c")
+
+    y = conv_block(y, 3, [128, 128, 384], stage=2, block='a', strides=(1, 1))
+    y = identity_block(y, 3, [128, 128, 512], stage=2, block='b')
+    y = identity_block(y, 3, [128, 128, 512], stage=2, block='c')
+    y = identity_block(y, 3, [128, 128, 512], stage=2, block='d')
+
+    y = conv_block(y, 3, [256, 256, 768], stage=2, block='a4')
+
+    bn_CbCr = layers.BatchNormalization(
+        axis=bn_axis, momentum=0.9, epsilon=1e-5, name='bn_cbcr')(CbCr)
+    cbcr = conv_block(bn_CbCr, 1, [256, 256, 1024],
+                      stage=2, block='a5', strides=(1, 1))
+
+    x = Concatenate(axis=-1)([y, cbcr])
+
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='b')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='c')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='d')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='e')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='f')
+
+    x = conv_block(x, 3, [512, 512, 2048], stage=5, block='a')
+    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='b')
+    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='c')
+
+    x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
+    x = layers.Dense(classes, activation='softmax',
+                     kernel_regularizer=l2(0.00005), name='fc1000')(x)
+
+    inputs = [img_input, img_input]
+
+    # Create model.
+    model = models.Model(inputs, x, name='resnet50_late_concat_rfa_thinner')
