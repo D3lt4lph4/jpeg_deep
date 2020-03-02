ea623a54b47e5ff550df3b01ebb9fcf495ec658d
BenjaminDeguerre <benjamin.deguerre@insa-rouen.fr>
2020-03-02 17:19:44 +0100

    error correctino

--- config/ssd_resnet/rgb/rgb_07+12/config_file.py
+++ config/ssd_resnet/rgb/rgb_07+12/config_file.py
@@ -132,10 +132,10 @@ class TrainingConfiguration(object):
 
     def prepare_training_generators(self):
         self._train_generator = VOCGenerator(batch_size=self.batch_size, shuffle=True, label_encoder=self.input_encoder,
-                                             transforms=self.train_tranformations, load_images_into_memory=None, images_path=self.train_sets)
+                                             transforms=self.train_tranformations, images_path=self.train_sets)
         self._train_generator.prepare_dataset()
         self._validation_generator = VOCGenerator(batch_size=self.batch_size, shuffle=True, label_encoder=self.input_encoder,
-                                                  transforms=self.validation_transformations, load_images_into_memory=None, images_path=self.validation_sets)
+                                                  transforms=self.validation_transformations, images_path=self.validation_sets)
         self._validation_generator.prepare_dataset(exclude_difficult=True)
         self.validation_steps = len(self._validation_generator)
 
