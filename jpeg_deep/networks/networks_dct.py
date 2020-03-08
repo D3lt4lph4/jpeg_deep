84d51f50b75875988828f484be148d461e6179bd
BenjaminDeguerre <benjamin.deguerre@insa-rouen.fr>
2020-03-08 09:08:34 +0100

    up network dct

--- jpeg_deep/networks/networks_dct.py
+++ jpeg_deep/networks/networks_dct.py
@@ -221,16 +221,19 @@ def vggd_dct_resize(classes=1000):
 
     norm_cbcr = BatchNormalization(
         name="b_norm_128", input_shape=input_shape_cbcr)(input_cbcr)
+    norm_cbcr = ResizeFeatures((14, 14))(norm_cbcr)
 
     # Block 1
     x = BatchNormalization(
         name="b_norm_64", input_shape=input_shape_y)(input_y)
+    x = ResizeFeatures((28, 28))(x)
 
     x = Conv2D(256, (3, 3),
                activation='relu',
                padding='same',
                kernel_regularizer=l2(0.0005),
                name='block1_conv1_dct_256')(x)
+    # x = ResizeFeatures((28, 28))(x)
 
     # Block 4
     x = Conv2D(512, (3, 3),
@@ -258,6 +261,7 @@ def vggd_dct_resize(classes=1000):
                padding='same',
                kernel_regularizer=l2(0.0005),
                name='block5_conv1_dct')(concat)
+
     x = Conv2D(512, (3, 3),
                activation='relu',
                padding='same',
@@ -269,8 +273,6 @@ def vggd_dct_resize(classes=1000):
                kernel_regularizer=l2(0.0005),
                name='block5_conv3')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
-
-    x = ResizeFeatures((7, 7))(x)
 
     # Classification block
     x = Flatten(name='flatten')(x)
@@ -441,9 +443,9 @@ def vggd_dct_deconv(classes=1000, input_shape=(28, 28)):
     input_cr = Input(shape=input_shape_cr)
 
     cb = Conv2DTranspose(64, kernel_size=(2, 2), strides=2,
-                         kernel_regularizer=l2(0.0005))(input_cb)
+                         kernel_regularizer=l2(0.0005), name="deconv_cb")(input_cb)
     cr = Conv2DTranspose(64, kernel_size=(2, 2), strides=2,
-                         kernel_regularizer=l2(0.0005))(input_cr)
+                         kernel_regularizer=l2(0.0005), name="deconv_cr")(input_cr)
 
     x = Concatenate(axis=-1)([input_y, cb, cr])
 
@@ -507,16 +509,25 @@ def vggd_dct_deconv_conv(classes=1000, input_shape=(28, 28)):
     # Returns
         A Keras model instance.
     """
-    input_shape_y = (*input_shape, 64)
-    input_shape_cbcr = (input_shape[0] // 2, input_shape[1] // 2, 128)
+    if input_shape is None:
+        input_shape_y = (None, None, 64)
+        input_shape_cb = (None, None, 64)
+        input_shape_cr = (None, None, 64)
+    else:
+        input_shape_y = (*input_shape, 64)
+        input_shape_cb = (input_shape[0] // 2, input_shape[1] // 2, 64)
+        input_shape_cr = (input_shape[0] // 2, input_shape[1] // 2, 64)
 
     input_y = Input(input_shape_y)
-    input_cbcr = Input(input_shape_cbcr)
+    input_cb = Input(shape=input_shape_cb)
+    input_cr = Input(shape=input_shape_cr)
 
-    cbcr = Conv2DTranspose(128, kernel_size=(
-        2, 2), stride=2, kernel_regularizer=l2(0.0005))(input_cbcr)
+    cb = Conv2DTranspose(64, kernel_size=(2, 2), strides=2,
+                         kernel_regularizer=l2(0.0005), name="deconv_cb")(input_cb)
+    cr = Conv2DTranspose(64, kernel_size=(2, 2), strides=2,
+                         kernel_regularizer=l2(0.0005), name="deconv_cr")(input_cr)
 
-    x = Concatenate([input_y, cbcr])
+    x = Concatenate(axis=-1)([input_y, cb, cr])
 
     x = BatchNormalization(
         name="b_norm", input_shape=input_shape_y)(x)
@@ -544,7 +555,7 @@ def vggd_dct_deconv_conv(classes=1000, input_shape=(28, 28)):
                activation='relu',
                padding='same',
                kernel_regularizer=l2(0.0005),
-               name='block5_conv1_dct')(concat)
+               name='block5_conv1_dct')(x)
     x = Conv2D(512, (3, 3),
                activation='relu',
                padding='same',
@@ -562,5 +573,159 @@ def vggd_dct_deconv_conv(classes=1000, input_shape=(28, 28)):
     x = Conv2D(4096, (1, 1), activation='relu', name='conv2d_2')(x)
     x = Conv2D(classes, (1, 1), activation='softmax', name='conv2d_3')(x)
     x = GlobalAveragePooling2D()(x)
+
+    return Model(inputs=[input_y, input_cb, input_cr], outputs=x)
+
+
+def vggd_dct_deconv_resize(classes=1000, input_shape=(28, 28)):
+    """Instantiates the VGG16 architecture.
+        classes: optional number of classes to classify images
+            into, only to be specified if `include_top` is True, and
+            if no `weights` argument is specified.
+    # Returns
+        A Keras model instance.
+    """
+    input_shape_y = (None, None, 64)
+    input_shape_cb = (None, None, 64)
+    input_shape_cr = (None, None, 64)
+
+    input_y = Input(input_shape_y)
+    input_cb = Input(shape=input_shape_cb)
+    input_cr = Input(shape=input_shape_cr)
+
+    cb = Conv2DTranspose(64, kernel_size=(2, 2), strides=2,
+                         kernel_regularizer=l2(0.0005))(input_cb)
+    cr = Conv2DTranspose(64, kernel_size=(2, 2), strides=2,
+                         kernel_regularizer=l2(0.0005))(input_cr)
+
+    x = Concatenate(axis=-1)([input_y, cb, cr])
+
+    x = BatchNormalization(
+        name="b_norm", input_shape=input_shape_y)(x)
+
+    # Block 4
+    x = Conv2D(512, (3, 3),
+               activation='relu',
+               padding='same',
+               kernel_regularizer=l2(0.0005),
+               name='block4_conv1_dct')(x)
+
+    x = Conv2D(512, (3, 3),
+               activation='relu',
+               padding='same',
+               kernel_regularizer=l2(0.0005),
+               name='block4_conv2')(x)
+
+    x = Conv2D(512, (3, 3),
+               activation='relu',
+               padding='same',
+               kernel_regularizer=l2(0.0005),
+               name='block4_conv3')(x)
+
+    # x = ResizeFeatures((28, 28))(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+
+    # Block 5
+    x = Conv2D(512, (3, 3),
+               activation='relu',
+               padding='same',
+               kernel_regularizer=l2(0.0005),
+               name='block5_conv1_dct')(x)
+
+    x = Conv2D(512, (3, 3),
+               activation='relu',
+               padding='same',
+               kernel_regularizer=l2(0.0005),
+               name='block5_conv2')(x)
+
+    x = Conv2D(512, (3, 3),
+               activation='relu',
+               padding='same',
+               kernel_regularizer=l2(0.0005),
+               name='block5_conv3')(x)
+    # x = ResizeFeatures((14, 14))(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+    x = ResizeFeatures((7, 7))(x)
+
+    # Classification block
+    x = Flatten(name='flatten')(x)
+    x = Dense(4096, activation='relu',
+              kernel_regularizer=l2(0.0005), name='fc1')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(4096, activation='relu',
+              kernel_regularizer=l2(0.0005), name='fc2')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(classes, activation='softmax', name='predictions')(x)
+
+    return Model(inputs=[input_y, input_cb, input_cr], outputs=x)
+
+
+def vggd_dct_y(classes=1000, input_shape=(28, 28)):
+    """Instantiates the VGG16 architecture.
+        classes: optional number of classes to classify images
+            into, only to be specified if `include_top` is True, and
+            if no `weights` argument is specified.
+    # Returns
+        A Keras model instance.
+    """
+    input_shape_y = (*input_shape, 64)
+
+    input_y = Input(input_shape_y)
+
+    # Block 1
+    x = BatchNormalization(
+        name="b_norm_64", input_shape=input_shape_y)(input_y)
+
+    x = Conv2D(256, (3, 3),
+               activation='relu',
+               padding='same',
+               kernel_regularizer=l2(0.0005),
+               name='block1_conv1_dct_256')(x)
+
+    # Block 4
+    x = Conv2D(512, (3, 3),
+               activation='relu',
+               padding='same',
+               kernel_regularizer=l2(0.0005),
+               name='block4_conv1_dct')(x)
+    x = Conv2D(512, (3, 3),
+               activation='relu',
+               padding='same',
+               kernel_regularizer=l2(0.0005),
+               name='block4_conv2')(x)
+    x = Conv2D(512, (3, 3),
+               activation='relu',
+               padding='same',
+               kernel_regularizer=l2(0.0005),
+               name='block4_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+
+    # Block 5
+    x = Conv2D(512, (3, 3),
+               activation='relu',
+               padding='same',
+               kernel_regularizer=l2(0.0005),
+               name='block5_conv1_dct')(x)
+    x = Conv2D(512, (3, 3),
+               activation='relu',
+               padding='same',
+               kernel_regularizer=l2(0.0005),
+               name='block5_conv2')(x)
+    x = Conv2D(512, (3, 3),
+               activation='relu',
+               padding='same',
+               kernel_regularizer=l2(0.0005),
+               name='block5_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+
+    # Classification block
+    x = Flatten(name='flatten')(x)
+    x = Dense(4096, activation='relu',
+              kernel_regularizer=l2(0.0005), name='fc1')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(4096, activation='relu',
+              kernel_regularizer=l2(0.0005), name='fc2')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(classes, activation='softmax', name='predictions')(x)
 
     return Model(inputs=[input_y, input_cbcr], outputs=x)
