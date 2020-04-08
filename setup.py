1ee824b1ed8ff87f7a3e2a6fd64bfe56e4d7684d
BenjaminDeguerre <benjamin.deguerre@insa-rouen.fr>
2020-04-08 19:46:40 +0200

    adding setup and removing useless config

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jpeg_deep',
+    version='1.0',
+    url='',
+    author='Deguerre Benjmain',
+    author_email='benjamin.deguerre@vinci-energies.net',
+    license='',
+    packages=find_packages()
+)
