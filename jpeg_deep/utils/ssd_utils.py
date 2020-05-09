f10ea43863793a85524200f56509cbbb8a1dd3ff
BenjaminDeguerre <benjamin.deguerre@insa-rouen.fr>
2020-05-09 16:04:49 +0200

    utils function

--- jpeg_deep/utils/ssd_utils.py
+++ jpeg_deep/utils/ssd_utils.py
@@ -21,30 +21,28 @@ from __future__ import division
 import numpy as np
 
 
-def convert_coordinates(tensor, start_index, conversion, border_pixels='half'):
+def convert_coordinates(tensor: object, start_index: int, conversion:str, border_pixels:str='half'):
     '''
     Convert coordinates for axis-aligned 2D boxes between two coordinate formats.
 
     Creates a copy of `tensor`, i.e. does not operate in place. Currently there are
     three supported coordinate formats that can be converted from and to each other:
-        1) (xmin, xmax, ymin, ymax) - the 'minmax' format
-        2) (xmin, ymin, xmax, ymax) - the 'corners' format
-        2) (cx, cy, w, h) - the 'centroids' format
 
-    Arguments:
-        tensor (array): A Numpy nD array containing the four consecutive coordinates
-            to be converted somewhere in the last axis.
-        start_index (int): The index of the first coordinate in the last axis of `tensor`.
-        conversion (str, optional): The conversion direction. Can be 'minmax2centroids',
-            'centroids2minmax', 'corners2centroids', 'centroids2corners', 'minmax2corners',
-            or 'corners2minmax'.
-        border_pixels (str, optional): How to treat the border pixels of the bounding boxes.
+    - (xmin, xmax, ymin, ymax) - the 'minmax' format
+    - (xmin, ymin, xmax, ymax) - the 'corners' format
+    - (cx, cy, w, h) - the 'centroids' format
+
+    # Arguments:
+        - tensor: A Numpy nD array containing the four consecutive coordinates to be converted somewhere in the last axis.
+        - start_index: The index of the first coordinate in the last axis of `tensor`.
+        - conversion: The conversion direction. Can be 'minmax2centroids', 'centroids2minmax', 'corners2centroids', 'centroids2corners', 'minmax2corners', or 'corners2minmax'.
+        - border_pixels: How to treat the border pixels of the bounding boxes.
             Can be 'include', 'exclude', or 'half'. If 'include', the border pixels belong
             to the boxes. If 'exclude', the border pixels do not belong to the boxes.
             If 'half', then one of each of the two horizontal and vertical borders belong
             to the boxex, but not the other.
 
-    Returns:
+    # Returns:
         A Numpy nD array, a copy of the input tensor with the converted coordinates
         in place of the original coordinates and the unaltered elements of the original
         tensor elsewhere.
@@ -103,40 +101,7 @@ def convert_coordinates(tensor, start_index, conversion, border_pixels='half'):
 
     return tensor1
 
-
-def convert_coordinates2(tensor, start_index, conversion):
-    '''
-    A matrix multiplication implementation of `convert_coordinates()`.
-    Supports only conversion between the 'centroids' and 'minmax' formats.
-
-    This function is marginally slower on average than `convert_coordinates()`,
-    probably because it involves more (unnecessary) arithmetic operations (unnecessary
-    because the two matrices are sparse).
-
-    For details please refer to the documentation of `convert_coordinates()`.
-    '''
-    ind = start_index
-    tensor1 = np.copy(tensor).astype(np.float)
-    if conversion == 'minmax2centroids':
-        M = np.array([[0.5, 0., -1.,  0.],
-                      [0.5, 0.,  1.,  0.],
-                      [0., 0.5,  0., -1.],
-                      [0., 0.5,  0.,  1.]])
-        tensor1[..., ind:ind+4] = np.dot(tensor1[..., ind:ind+4], M)
-    elif conversion == 'centroids2minmax':
-        M = np.array([[1., 1.,  0., 0.],
-                      [0., 0.,  1., 1.],
-                      [-0.5, 0.5,  0., 0.],
-                      [0., 0., -0.5, 0.5]])  # The multiplicative inverse of the matrix above
-        tensor1[..., ind:ind+4] = np.dot(tensor1[..., ind:ind+4], M)
-    else:
-        raise ValueError(
-            "Unexpected conversion value. Supported values are 'minmax2centroids' and 'centroids2minmax'.")
-
-    return tensor1
-
-
-def intersection_area(boxes1, boxes2, coords='centroids', mode='outer_product', border_pixels='half'):
+def intersection_area(boxes1:object, boxes2:object, coords:str='centroids', mode:str='outer_product', border_pixels:str='half'):
     '''
     Computes the intersection areas of two sets of axis-aligned 2D rectangular boxes.
 
@@ -149,21 +114,21 @@ def intersection_area(boxes1, boxes2, coords='centroids', mode='outer_product', 
     of the `mode` argument for details.
 
     Arguments:
-        boxes1 (array): Either a 1D Numpy array of shape `(4, )` containing the coordinates for one box in the
+        - boxes1: Either a 1D Numpy array of shape `(4, )` containing the coordinates for one box in the
             format specified by `coords` or a 2D Numpy array of shape `(m, 4)` containing the coordinates for `m` boxes.
             If `mode` is set to 'element_wise', the shape must be broadcast-compatible with `boxes2`.
-        boxes2 (array): Either a 1D Numpy array of shape `(4, )` containing the coordinates for one box in the
+        - boxes2: Either a 1D Numpy array of shape `(4, )` containing the coordinates for one box in the
             format specified by `coords` or a 2D Numpy array of shape `(n, 4)` containing the coordinates for `n` boxes.
             If `mode` is set to 'element_wise', the shape must be broadcast-compatible with `boxes1`.
-        coords (str, optional): The coordinate format in the input arrays. Can be either 'centroids' for the format
+        - coords : The coordinate format in the input arrays. Can be either 'centroids' for the format
             `(cx, cy, w, h)`, 'minmax' for the format `(xmin, xmax, ymin, ymax)`, or 'corners' for the format
             `(xmin, ymin, xmax, ymax)`.
-        mode (str, optional): Can be one of 'outer_product' and 'element-wise'. In 'outer_product' mode, returns an
+        - mode : Can be one of 'outer_product' and 'element-wise'. In 'outer_product' mode, returns an
             `(m,n)` matrix with the intersection areas for all possible combinations of the `m` boxes in `boxes1` with the
             `n` boxes in `boxes2`. In 'element-wise' mode, returns a 1D array and the shapes of `boxes1` and `boxes2`
             must be boadcast-compatible. If both `boxes1` and `boxes2` have `m` boxes, then this returns an array of
             length `m` where the i-th position contains the intersection area of `boxes1[i]` with `boxes2[i]`.
-        border_pixels (str, optional): How to treat the border pixels of the bounding boxes.
+        - border_pixels : How to treat the border pixels of the bounding boxes.
             Can be 'include', 'exclude', or 'half'. If 'include', the border pixels belong
             to the boxes. If 'exclude', the border pixels do not belong to the boxes.
             If 'half', then one of each of the two horizontal and vertical borders belong
@@ -259,9 +224,30 @@ def intersection_area(boxes1, boxes2, coords='centroids', mode='outer_product', 
         return side_lengths[:, 0] * side_lengths[:, 1]
 
 
-def intersection_area_(boxes1, boxes2, coords='corners', mode='outer_product', border_pixels='half'):
+def intersection_area_(boxes1:object, boxes2:object, coords:str='corners', mode:str='outer_product', border_pixels:str='half'):
     '''
     The same as 'intersection_area()' but for internal use, i.e. without all the safety checks.
+
+    # Arguments:
+        - boxes1: Either a 1D Numpy array of shape `(4, )` containing the coordinates for one box in the
+            format specified by `coords` or a 2D Numpy array of shape `(m, 4)` containing the coordinates for `m` boxes.
+            If `mode` is set to 'element_wise', the shape must be broadcast-compatible with `boxes2`.
+        - boxes2: Either a 1D Numpy array of shape `(4, )` containing the coordinates for one box in the
+            format specified by `coords` or a 2D Numpy array of shape `(n, 4)` containing the coordinates for `n` boxes.
+            If `mode` is set to 'element_wise', the shape must be broadcast-compatible with `boxes1`.
+        - coords: The coordinate format in the input arrays. Can be either 'centroids' for the format
+            `(cx, cy, w, h)`, 'minmax' for the format `(xmin, xmax, ymin, ymax)`, or 'corners' for the format
+            `(xmin, ymin, xmax, ymax)`.
+        - mode: Can be one of 'outer_product' and 'element-wise'. In 'outer_product' mode, returns an
+            `(m,n)` matrix with the IoU overlaps for all possible combinations of the `m` boxes in `boxes1` with the
+            `n` boxes in `boxes2`. In 'element-wise' mode, returns a 1D array and the shapes of `boxes1` and `boxes2`
+            must be boadcast-compatible. If both `boxes1` and `boxes2` have `m` boxes, then this returns an array of
+            length `m` where the i-th position contains the IoU overlap of `boxes1[i]` with `boxes2[i]`.
+        - border_pixels: How to treat the border pixels of the bounding boxes.
+            Can be 'include', 'exclude', or 'half'. If 'include', the border pixels belong
+            to the boxes. If 'exclude', the border pixels do not belong to the boxes.
+            If 'half', then one of each of the two horizontal and vertical borders belong
+            to the boxex, but not the other.
     '''
 
     m = boxes1.shape[0]  # The number of boxes in `boxes1`
@@ -318,7 +304,7 @@ def intersection_area_(boxes1, boxes2, coords='corners', mode='outer_product', b
         return side_lengths[:, 0] * side_lengths[:, 1]
 
 
-def iou(boxes1, boxes2, coords='centroids', mode='outer_product', border_pixels='half'):
+def iou(boxes1: object, boxes2: object, coords:str='centroids', mode:str='outer_product', border_pixels:str='half'):
     '''
     Computes the intersection-over-union similarity (also known as Jaccard similarity)
     of two sets of axis-aligned 2D rectangular boxes.
@@ -331,28 +317,28 @@ def iou(boxes1, boxes2, coords='centroids', mode='outer_product', border_pixels=
     In 'element-wise' mode, `m` and `n` must be broadcast-compatible. Refer to the explanation
     of the `mode` argument for details.
 
-    Arguments:
-        boxes1 (array): Either a 1D Numpy array of shape `(4, )` containing the coordinates for one box in the
+    # Arguments:
+        - boxes1: Either a 1D Numpy array of shape `(4, )` containing the coordinates for one box in the
             format specified by `coords` or a 2D Numpy array of shape `(m, 4)` containing the coordinates for `m` boxes.
             If `mode` is set to 'element_wise', the shape must be broadcast-compatible with `boxes2`.
-        boxes2 (array): Either a 1D Numpy array of shape `(4, )` containing the coordinates for one box in the
+        - boxes2: Either a 1D Numpy array of shape `(4, )` containing the coordinates for one box in the
             format specified by `coords` or a 2D Numpy array of shape `(n, 4)` containing the coordinates for `n` boxes.
             If `mode` is set to 'element_wise', the shape must be broadcast-compatible with `boxes1`.
-        coords (str, optional): The coordinate format in the input arrays. Can be either 'centroids' for the format
+        - coords: The coordinate format in the input arrays. Can be either 'centroids' for the format
             `(cx, cy, w, h)`, 'minmax' for the format `(xmin, xmax, ymin, ymax)`, or 'corners' for the format
             `(xmin, ymin, xmax, ymax)`.
-        mode (str, optional): Can be one of 'outer_product' and 'element-wise'. In 'outer_product' mode, returns an
+        - mode: Can be one of 'outer_product' and 'element-wise'. In 'outer_product' mode, returns an
             `(m,n)` matrix with the IoU overlaps for all possible combinations of the `m` boxes in `boxes1` with the
             `n` boxes in `boxes2`. In 'element-wise' mode, returns a 1D array and the shapes of `boxes1` and `boxes2`
             must be boadcast-compatible. If both `boxes1` and `boxes2` have `m` boxes, then this returns an array of
             length `m` where the i-th position contains the IoU overlap of `boxes1[i]` with `boxes2[i]`.
-        border_pixels (str, optional): How to treat the border pixels of the bounding boxes.
+        - border_pixels: How to treat the border pixels of the bounding boxes.
             Can be 'include', 'exclude', or 'half'. If 'include', the border pixels belong
             to the boxes. If 'exclude', the border pixels do not belong to the boxes.
             If 'half', then one of each of the two horizontal and vertical borders belong
             to the boxex, but not the other.
 
-    Returns:
+    # Returns:
         A 1D or 2D Numpy array (refer to the `mode` argument for details) of dtype float containing values in [0,1],
         the Jaccard similarity of the boxes in `boxes1` and `boxes2`. 0 means there is no overlap between two given
         boxes, 1 means their coordinates are identical.
